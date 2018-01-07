bede8cf669f3bff2a9bee889343c2f45ecd1cd9b
MatriX <MatriX@192.168.1.20>
2018-01-07 15:53:47 +0100

    Pierwsze pola.java

--- Zadania/src/Organizer_Zadan/OrganizerZadan.java
+++ Zadania/src/Organizer_Zadan/OrganizerZadan.java
@@ -5,13 +5,20 @@
  */
 package Organizer_Zadan;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 /**
  *
  * @author MatriX
  */
-public class OrganizerZadan {
+public class OrganizerZadan implements ActionListener {
+   JTextField t1;
+   JButton b1;
+   JButton b2;
        public static void main(String[] args)          
    {                                               
       //do wersji 1.4                              
@@ -30,8 +37,13 @@ public class OrganizerZadan {
 
     private void init() {
       t1=new JTextField(6);                             
-      b1=new JButton("^2");                             
-      b2=new JButton("clear");
+      b1=new JButton("ADD");                             
+      b2=new JButton("DELETE");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
