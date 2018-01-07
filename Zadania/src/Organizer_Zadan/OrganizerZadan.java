bf93abb90b472fcb81be1e6d08f7a8df71922e18
MatriX <MatriX@192.168.1.20>
2018-01-07 15:50:17 +0100

    Poczatki programowania.java

--- Zadania/src/Organizer_Zadan/OrganizerZadan.java
+++ Zadania/src/Organizer_Zadan/OrganizerZadan.java
@@ -5,10 +5,33 @@
  */
 package Organizer_Zadan;
 
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author MatriX
  */
 public class OrganizerZadan {
+       public static void main(String[] args)          
+   {                                               
+      //do wersji 1.4                              
+      //new Demo().init();                         
+ 
+      //od wersji 1.5                              
+      SwingUtilities.invokeLater(new Runnable()    
+      {                                            
+         @Override
+         public void run()                         
+         {                                         
+            new OrganizerZadan().init();                     
+         }                                         
+      });                                          
+   }      
+
+    private void init() {
+      t1=new JTextField(6);                             
+      b1=new JButton("^2");                             
+      b2=new JButton("clear");
+    }
     
 }
