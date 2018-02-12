5c3e3061898bc5eac07635eff0c31fc5ebc4d104
mbrodziak <matousb96@gmail.com>
2018-02-12 18:50:17 +0100

    Dodanie nowego pola z datą

--- Zadania/src/Organizer_Zadan/OrganizerZadan.java
+++ Zadania/src/Organizer_Zadan/OrganizerZadan.java
@@ -11,10 +11,13 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import java.util.*;
 
 
 /**
@@ -24,9 +27,13 @@ import javax.swing.JTextField;
 public class OrganizerZadan implements ActionListener {
    JTextField Pole;
    JTextArea Pole2;
+   JTextField Pole3;
    JButton Przycisk;
    JButton Przycisk2;
    JButton Przycisk3;
+   JButton Przycisk4;
+   
+  
 
 
     public void init() {
@@ -37,41 +44,58 @@ public class OrganizerZadan implements ActionListener {
       GridBagConstraints gbc = new GridBagConstraints();                          
       gbc.fill = GridBagConstraints.HORIZONTAL;                                   
       c.setLayout(gbl); 
-      
-      
- 
+      Date data = new Date();
+      DateFormat dateFormat = new SimpleDateFormat("dd.MM.YYYY");
+      java.util.Date date = new java.util.Date();
+      String currDate = dateFormat.format(date);
+      System.out.print(currDate);
+     
+     
       Pole = new JTextField(20);
       Pole.addActionListener(this);                       
-      Pole.setFocusable(false);                         
+      Pole.setFocusable(false);    
       gbc.gridx = 0;                                                              
-      gbc.gridy = 0;                                                              
+      gbc.gridy = 1;                                                              
       gbc.gridwidth = 1;                                                          
       gbc.ipadx = 0;                                                              
       gbc.ipady = 0;                                                              
-      gbc.insets = new Insets(5,5,0,0);                                           
+      gbc.insets = new Insets(5,5,5,5);                                           
       gbl.setConstraints(Pole, gbc);                                               
       c.add(Pole);  
       
       Pole2 = new JTextArea(20,20);
       Pole2.setFocusable(false);                                                   
       gbc.gridx = 0;                                                              
-      gbc.gridy = 1;                                                              
+      gbc.gridy = 2;                                                              
       gbc.gridwidth = 1;                                                          
       gbc.ipadx = 0;                                                              
       gbc.ipady = 0;                                                              
-      gbc.insets = new Insets(5,5,0,0);                                           
+      gbc.insets = new Insets(5,5,5,5);                                           
       gbl.setConstraints(Pole2, gbc);                                               
       c.add(Pole2);  
+      
+      Pole3 = new JTextField(20);                      
+      Pole3.setFocusable(false);
+      Pole3.setText(currDate);
+      gbc.gridx = 0;                                                              
+      gbc.gridy = 0;                                                              
+      gbc.gridwidth = 1;                                                          
+      gbc.ipadx = 0;                                                              
+      gbc.ipady = 0;                                                              
+      gbc.insets = new Insets(5,5,5,5);                                           
+      gbl.setConstraints(Pole3, gbc);    
+      c.add(Pole3);
+ 
       
       Przycisk = new JButton("ADD");
       Przycisk.addActionListener(this);                       
       Przycisk.setFocusable(false);                                                   
       gbc.gridx = 1;                                                              
-      gbc.gridy = 0;                                                              
+      gbc.gridy = 1;                                                              
       gbc.gridwidth = 1;                                                          
       gbc.ipadx = 0;                                                              
       gbc.ipady = 0;                                                              
-      gbc.insets = new Insets(5,5,5,0);                                           
+      gbc.insets = new Insets(5,5,5,5);                                           
       gbl.setConstraints(Przycisk, gbc);                                               
       c.add(Przycisk);  
       
@@ -79,7 +103,7 @@ public class OrganizerZadan implements ActionListener {
       Przycisk2.addActionListener(this);                       
       Przycisk2.setFocusable(false);                                                   
       gbc.gridx = 2;                                                              
-      gbc.gridy = 0;                                                              
+      gbc.gridy = 1;                                                              
       gbc.gridwidth = 1;                                                          
       gbc.ipadx = 0;                                                              
       gbc.ipady = 0;                                                              
@@ -87,10 +111,10 @@ public class OrganizerZadan implements ActionListener {
       gbl.setConstraints(Przycisk2, gbc);                                               
       c.add(Przycisk2);
       
-      Przycisk3 = new JButton("Back");
+      Przycisk3 = new JButton("BACK");
       Przycisk3.addActionListener(this);
       Przycisk3.setFocusable(false);
-      gbc.gridx = 3;
+      gbc.gridx = 1;
       gbc.gridy = 0;
       gbc.gridwidth = 1;
       gbc.ipadx = 0;
@@ -98,6 +122,7 @@ public class OrganizerZadan implements ActionListener {
       gbc.insets = new Insets(5,5,5,5);
       gbl.setConstraints(Przycisk3, gbc); 
       c.add(Przycisk3);
+     
       
       
  
